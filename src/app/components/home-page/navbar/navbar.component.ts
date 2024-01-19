de57fdbc3395304156ab56dea5d9fcd73ebc7dd8
dejanV91 <dejanvukoicic91@gmail.com>
2024-01-19 17:20:32 +0100

    fix navbar

--- src/app/components/home-page/navbar/navbar.component.ts
+++ src/app/components/home-page/navbar/navbar.component.ts
@@ -9,15 +9,27 @@ export class NavbarComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    let isProjectsScroll = localStorage.getItem('projectsPage') ? true : false;
-    if (isProjectsScroll) {
-    }
-    localStorage.removeItem('projectsPage');
-  }
+    let section = document.querySelectorAll('.container');
+    let navLinks = document.querySelectorAll('header nav a');
 
-  changeNav(index: number, fragment: string) {
-    let active = document.querySelector('.active') as HTMLDivElement;
-    active.style.transform = 'translate3d(' + index * 100 + '%,0,0)';
-    document.getElementById(fragment)?.scrollIntoView({ behavior: 'smooth' });
+    window.onscroll = () => {
+      section.forEach((sec) => {
+        let current = sec as HTMLDivElement;
+
+        let top = window.scrollY;
+        let offset = current.offsetTop;
+        let height = current.offsetHeight;
+        let id = current.getAttribute('id');
+
+        if (top >= offset - 300) {
+          navLinks.forEach((links) => {
+            links.classList.remove('active');
+            document
+              .querySelector(`header nav a[href*=${id}]`)
+              ?.classList.add('active');
+          });
+        }
+      });
+    };
   }
 }
